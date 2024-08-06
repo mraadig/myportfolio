5429cbce644234650b7b80f710033b345aa9d98e
Aditya Kumar Gupta <adityakumargupta2101@gmail.com>
2024-08-06 22:34:41 +0530

    Update Vite configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Replace 'myportfolio' with your actual GitHub repository name
 export default defineConfig({
+  base: '/myportfolio/', 
   plugins: [react()],
 })
+
