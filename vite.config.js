5dbf86732381297ad51b9c8051e8753184fd8900
Aditya Kumar Gupta <adityakumargupta2101@gmail.com>
2024-08-18 15:33:32 +0530

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Replace 'myportfolio' with your actual GitHub repository name
-export default defineConfig({
-  base: '/myportfolio/', 
-  plugins: [react()],
-})
-
